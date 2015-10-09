9a9a61d2a4429bfe292bac9fb1eb5eb374265609
haojunyu <haojunyu2012@gmail.com>
2015-10-09 13:15:05 +0800

    c模板文件

---
+++ vim/.vim/template/c.c
@@ -0,0 +1,18 @@
+///@file    c.c
+///@brief   c程序的模板文件
+///@author  hjy
+
+#include<stdio.h>
+
+int main(){
+    
+    
+    return 0;
+}
+
+
+///@brief   函数简要介绍
+///@param   a   参数a的描述
+///@return  函数返回变量
+///@see     参考其他相关函数
+///note     注意问题
