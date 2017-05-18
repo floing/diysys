e93c916cf0c507c0853c0db9e6fd863d1d8980d5
haojunyu <haojunyu2012@gmail.com>
2017-05-18 17:40:42 +0800

    add python source code

--- vim/.vim/template/python.py
+++ vim/.vim/template/python.py
@@ -6,10 +6,10 @@ import sys
 
 # 代码写在main()里面
 def main():
-    # 命令行的参数在 sys.argv[1], sys.argv[2] ...
-    # sys.argv[0] 表示脚本名称
-    print 'Hello there', sys.argv[0]
+  # 命令行的参数在 sys.argv[1], sys.argv[2] ...
+  # sys.argv[0] 表示脚本名称
+  print 'Hello there', sys.argv[0]
 
 # 调用main()函数来启动程序的样板
 if __name__ == '__main__':
-    main()
+  main()
