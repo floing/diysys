bc2e07828c8b430158d8fc9541a1ed20ce3dad6e
haojunyu <haojunyu2012@gmail.com>
2019-04-15 21:00:17 +0800

    vim add vim-template

---
+++ vim/.vim/template/=template=.c
@@ -0,0 +1,16 @@
+/**
+ * @file      : %FFILE%
+ * @brief     : %HERE%
+ *
+ * @author    : %USER% <%MAIL%>
+ * @date      : %DATE% %TIME%
+ * @copyright : hjy, license %LICENSE%
+ */
+
+#include "%FILE%.h"
+#include<stdio.h>
+
+int main(){
+    
+    return 0;
+}
---
+++ vim/.vim/template/=template=.h
@@ -0,0 +1,15 @@
+/**
+ * @file      : %FFILE%
+ * @brief     : %HERE%
+ *
+ * @author    : %USER% <%MAIL%>
+ * @date      : %DATE% %TIME%
+ * @copyright : hjy, license %LICENSE%
+ */
+
+#ifndef %GUARD%
+#define %GUARD%
+
+detail
+
+#endif /* !%GUARD% */
